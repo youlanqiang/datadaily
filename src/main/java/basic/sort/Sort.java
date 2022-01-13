b4305285d4ccd2dd89d99890cda9ececf332f18b
youlanqiang <youlanqiang@hotmail.com>
2022-01-14 05:01:19 +0800

    选择排序算法更新.

--- src/main/java/basic/sort/Sort.java
+++ src/main/java/basic/sort/Sort.java
@@ -13,34 +13,38 @@ import utils.RandomUtils;
 public class Sort {
 
     public static void main(String[] args) {
-        Integer[] arr = {88, 71,45,422,32,22,734, 3};
         //insertionSort(arr); //插入排序
+        Integer[] arr = RandomUtils.randomArray(100);
         Stopwatch stopwatch = Stopwatch.createStarted();
-        bubbleSort(RandomUtils.randomArray(1000), false);  //冒泡排序
+        //bubbleSort(RandomUtils.randomArray(5), false);  //冒泡排序
+        //insertionSort(arr, true);  // 插入排序
+        selectionSort(arr, true); //选择排序
         stopwatch.stop();
-        System.out.println(stopwatch.toString());
+        log.info("耗时:{}",stopwatch.toString());
         
 
     }
 
-    /**
-     * 插入排序
-     */
-    public static <T extends Comparable<? super T>>  void insertionSort(T[] arr, boolean debug){
+    public static <T extends Comparable<? super T>> void selectionSort(T[] arr, boolean debug){
         if(debug){
             log.info("原始数组:{} 数组大小:{}", arr, arr.length);
         }
 
-        int j;
-
-        for (int p = 1; p < arr.length; p++){
-            T tmp = arr[p];
-            for (j = p; j > 0 && tmp.compareTo(arr[j - 1]) < 0; j--) {
-                arr[j] = arr[j - 1];
+        for (int i = 0; i < arr.length - 1; i++) {
+            T min = arr[i];
+            int minIndex = i;
+            for(int j = i + 1; j < arr.length; j++){
+                if(min.compareTo(arr[j]) > 0){
+                    min = arr[j];
+                    minIndex = j;
+                }
             }
-            arr[j] = tmp;
+            T temp = arr[i];
+            arr[i] = arr[minIndex];
+            arr[minIndex] = temp;
+
             if(debug){
-                log.info("第{}次排序,数组:{}",  p, arr);
+                log.info("第{}次排序,数组:{}", i+1, arr);
             }
         }
     }
@@ -62,6 +66,29 @@ public class Sort {
             }
             if(debug){
                 log.info("第{}次排序,数组:{}",  arr.length - i, arr);
+            }
+        }
+    }
+
+
+    /**
+     * 插入排序
+     */
+    public static <T extends Comparable<? super T>>  void insertionSort(T[] arr, boolean debug){
+        if(debug){
+            log.info("原始数组:{} 数组大小:{}", arr, arr.length);
+        }
+
+        int j;
+
+        for (int p = 1; p < arr.length; p++){
+            T tmp = arr[p];
+            for (j = p; j > 0 && tmp.compareTo(arr[j - 1]) < 0; j--) {
+                arr[j] = arr[j - 1];
+            }
+            arr[j] = tmp;
+            if(debug){
+                log.info("第{}次排序,数组:{}",  p, arr);
             }
         }
     }
